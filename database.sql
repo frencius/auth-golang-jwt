a4c5b635019865018a9ba7f599d81797f8467007
Frencius <frencius.frencius@mapan.id>
2023-08-09 16:25:49 +0700

    initial db model

--- database.sql
+++ database.sql
@@ -10,8 +10,21 @@
 /** This is test table. Remove this table and replace with your own tables. */
 CREATE TABLE test (
 	id serial PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	name VARCHAR ( 50 ) UNIQUE NOT NULL
 );
 
 INSERT INTO test (name) VALUES ('test1');
 INSERT INTO test (name) VALUES ('test2');
+
+
+CREATE TABLE user (
+	id                      UUID PRIMARY KEY,
+	full_name               VARCHAR (100) NOT NULL,
+  phone_number            VARCHAR (15) UNIQUE NOT NULL,
+  password                VARCHAR (255) NOT NULL,
+  success_login_counter   int NOT NULL DEFAULT 0,
+  created_at              timestamptz		NOT NULL DEFAULT now(),
+	updated_at              timestamptz		NOT NULL DEFAULT now(),
+	created_by              varchar(100)	NOT NULL DEFAULT 'system'::character varying,
+	updated_by              varchar(100)	NOT NULL DEFAULT 'system'::character varying
+);
